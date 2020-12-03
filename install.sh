0da90b67ee20d9e9bfb1152657f7db752001918f
Fabio Mendes <fabio.mendes@rubylife.com>
2020-12-03 17:08:25 -0500

    Updating install script

--- install.sh
+++ install.sh
@@ -5,27 +5,27 @@
 
 echo "Installing shell goodies"
 
-if [ ! -n "$ZSH" ]; then
+if [[ ! -n "$ZSH" ]]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 VIM_PACK="$HOME/.vim/pack/dist/start"
-if [ ! -d $VIM_PACK/vim-airline ]; then
+if [[ ! -d $VIM_PACK/vim-airline ]]; then
   git clone https://github.com/vim-airline/vim-airline $VIM_PACK/vim-airline
   git clone https://github.com/vim-airline/vim-airline-themes $VIM_PACK/vim-airline-themes
 fi
 
 
-[ -f $HOME/.toprc ] || ln -s .shell-goodies/dot-rc/toprc $HOME/.toprc
-[ -f $HOME/.vimrc ] || ln -s .shell-goodies/vim/vimrc $HOME/.vimrc
-[ -f $HOME/.tmux.conf ] || ln -s .shell-goodies/dot-rc/tmux.conf $HOME/.tmux.conf
+[[ -f $HOME/.toprc ]] || ln -s .shell-goodies/dot-rc/toprc $HOME/.toprc
+[[ -f $HOME/.vimrc ]] || ln -s .shell-goodies/vim/vimrc $HOME/.vimrc
+[[ -f $HOME/.tmux.conf ]] || ln -s .shell-goodies/dot-rc/tmux.conf $HOME/.tmux.conf
 
-if [ ! -f $HOME/.gdbinit ]; then
+if [[ ! -f $HOME/.gdbinit ]]; then
   ln -s .shell-goodies/dot-rc/gdbinit $HOME/.gdbinit
   curl -L -o ~/.gdb-dashboard https://git.io/.gdbinit
 fi
 
-[ "$(uname)" = "Linux" ] && INPL="-i.bkp" || INPL="-i .bkp"
+[[ $(uname) == "Linux" ]] && INPL="-i.bkp" || INPL="-i .bkp"
 sed $INPL \
   -e 's/ZSH_THEME=".*"/ZSH_THEME="fabio"/' \
   -e 's/# ZSH_CUSTOM=.*/ZSH_CUSTOM=$HOME\/.shell-goodies\/zsh/' \
