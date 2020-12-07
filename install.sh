1d68d7adfa1c2e6f2304f3a742db2287bbb40178
Fabio Mendes <fabiojmendes@gmail.com>
2020-12-07 22:34:29 +0000

    Adding NERDtree to vim

--- install.sh
+++ install.sh
@@ -12,9 +12,15 @@ fi
 VIM_PACK="$HOME/.vim/pack/dist/start"
 if [[ ! -d $VIM_PACK/vim-airline ]]; then
   git clone https://github.com/vim-airline/vim-airline $VIM_PACK/vim-airline
+  vim -u NONE -c ":helptags $VIM_PACK/vim-airline/doc" -c q
   git clone https://github.com/vim-airline/vim-airline-themes $VIM_PACK/vim-airline-themes
+  vim -u NONE -c ":helptags $VIM_PACK/vim-airline-themes/doc" -c q
 fi
 
+if [[ ! -d $VIM_PACK/nerdtree ]]; then
+  git clone https://github.com/preservim/nerdtree.git $VIM_PACK/nerdtree
+  vim -u NONE -c "helptags $VIM_PACK/nerdtree/doc" -c q
+fi
 
 [[ -f $HOME/.toprc ]] || ln -s .shell-goodies/dot-rc/toprc $HOME/.toprc
 [[ -f $HOME/.vimrc ]] || ln -s .shell-goodies/vim/vimrc $HOME/.vimrc
