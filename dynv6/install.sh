d8f1b8ec972d70a5b87c5fe2a783fb3da63ff847
Fabio Mendes <fabiojmendes@gmail.com>
2021-04-26 18:45:27 -0400

    Adding systemd unit files and instalation script

---
+++ dynv6/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+if ! command -v dig &> /dev/null; then
+  echo "dig command is required"
+  echo "Install it using apt install dnsutils"
+  echo "Installation failed"
+  exit 1
+fi
+
+if [[ -f /etc/default/dynv6 ]]; then
+  echo "Config file already exists, skipping"
+else
+  echo "Copying config file"
+  cp ./dynv6 /etc/default
+fi
+
+echo "Copy dynv6 main script"
+cp ./dynv6.sh /usr/local/sbin
+
+echo "Copy dynv6 systemd scripts"
+cp ./dynv6.{service,timer} /etc/systemd/system
+
+echo "Reload systemd"
+systemctl daemon-reload
+systemctl enable dynv6.timer
+systemctl start dynv6.timer
+
+echo "Instalation completed"
